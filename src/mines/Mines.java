76f34d3b55b55c1853f042f534c53558f8eae41c
rachalabidi <rashalabidi@gmail.com>
2023-05-10 01:13:58 +0200

    On a remplacé JFrame par WindowConstants dans Mines.java et on a importer import javax.swing.WindowConstants;

--- src/mines/Mines.java
+++ src/mines/Mines.java
@@ -2,22 +2,22 @@ package mines;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
+import javax.swing.WindowConstants;
 
 // Source: http://zetcode.com/tutorials/javagamestutorial/minesweeper/
 
 public class Mines extends JFrame {
 	private static final long serialVersionUID = 4772165125287256837L;
 	
-	private final int WIDTH = 250;
-    private final int HEIGHT = 290;
+	private static  final int WIDTH = 250;
+    private static  final int HEIGHT = 290;
 
     private JLabel statusbar;
     
     public Mines() {
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(null);
         setTitle("Minesweeper");
